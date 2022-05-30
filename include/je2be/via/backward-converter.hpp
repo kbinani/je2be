40425fd7edb68173609d797cd9ecbef1749f5ce4
kbinani <kbinani.bt@gmail.com>
2022-05-31 05:15:47 +0900

    feat: Create version list supported by the via

--- include/je2be/via/backward-converter.hpp
+++ include/je2be/via/backward-converter.hpp
@@ -4,6 +4,23 @@ namespace je2be::via {
 
 class BackwardConverter {
   BackwardConverter() = delete;
+
+public:
+  enum class Version : uint8_t {
+    Version1_19 = 1,
+    Version1_18 = 2,
+    Version1_17 = 3,
+    Version1_16_2 = 4,
+    Version1_16 = 5,
+    Version1_15 = 6,
+    Version1_14 = 7,
+    Version1_13_2 = 8,
+    Version1_13 = 9,
+    Version1_12 = 10,
+    Version1_11 = 11,
+    Version1_10 = 12,
+    Version1_9_4 = 13,
+  };
 };
 
 } // namespace je2be::via
--- tool/create-backward-converter.cpp
+++ tool/create-backward-converter.cpp
@@ -1,14 +1,49 @@
 #include <stdio.h>
 
-#include <nlohmann/json.hpp>
-
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <vector>
 
+#include <nlohmann/json.hpp>
+
 namespace {
-std::string Replace(std::string const &target, std::string const &search, std::string const &replace) {
+
+static std::vector<std::string> Split(std::string const &sentence, char delimiter) {
+  std::istringstream input(sentence);
+  std::vector<std::string> tokens;
+  for (std::string line; std::getline(input, line, delimiter);) {
+    tokens.push_back(line);
+  }
+  return tokens;
+}
+
+inline std::string LTrim(std::string const &s, std::string const &left) {
+  if (left.empty()) {
+    return s;
+  }
+  std::string ret = s;
+  while (ret.starts_with(left)) {
+    ret = ret.substr(left.size());
+  }
+  return ret;
+}
+
+inline std::string RTrim(std::string const &s, std::string const &right) {
+  if (right.empty()) {
+    return s;
+  }
+  std::string ret = s;
+  while (ret.ends_with(right)) {
+    ret = ret.substr(0, ret.size() - right.size());
+  }
+  return ret;
+}
+
+inline std::string Trim(std::string const &left, std::string const &s, std::string const &right) { return RTrim(LTrim(s, left), right); }
+
+inline std::string Replace(std::string const &target, std::string const &search, std::string const &replace) {
   using namespace std;
   if (search.empty()) {
     return target;
@@ -28,7 +63,127 @@ std::string Replace(std::string const &target, std::string const &search, std::s
   }
   return ret;
 }
+
+static std::string Indent(std::string const &input, std::string const &indentString) {
+  auto lines = Split(input, '\n');
+  std::ostringstream ss;
+  for (auto const &line : lines) {
+    ss << indentString << line << std::endl;
+  }
+  return RTrim(RTrim(ss.str(), " "), "\n");
+}
+
+static std::string Trim(std::string const &v) {
+  return Trim("\n", Trim(" ", v, " "), "\n");
+}
+
+std::string classFormat = R"(
+#pragma once
+
+namespace je2be::via {
+
+class BackwardConverter {
+  BackwardConverter() = delete;
+
+public:
+  enum class Version : uint8_t {
+@{enum_definition}
+  };
+};
+
+} // namespace je2be::via
+)";
+
+std::string tableFunctionFormat = R"(
+static std::shared_ptr<mcfile::je::Block const> Convert@{version_pair}(std::shared_ptr<mcfile::je::Block const> const& input) {
+  using namespace std;
+  static unique_ptr<unordered_map<string, CompoundTagPtr> const> sDedicatedConvertMap(CreateDedicatedConvertMap@{version_pair}());
+  static unique_ptr<unordered_map<string, string> const> sRenameConvertMap(CreateRenameConvertMap@{version_pair}());
+
+  if (!input) {
+    return nullptr;
+  }
+
+  auto const& dedicated = *sDedicatedConvertMap;
+  auto const& rename = *sRenameConvertMap;
+
+  if (auto found = rename.find(input->fName); found != rename.end()) {
+    map<string, string> props(input->fProperties);
+    return make_shared<mcfile::je::Block const>(found->second, props);
+  }
+
+  auto str = input->toString();
+  if (auto found = dedicated.find(str); found != dedicated.end()) {
+    return mcfile::je::Block::FromCompoundTag(*found->second);
+  }
+
+  return input;
+}
+
+static std::unordered_map<std::string, CompoundTagPtr> const* CreateDedicatedConvertMap@{version_pair}() {
+  using namespace std;
+  auto t = new unordered_map<string, CompoundTagPtr>();
+  auto &s = *t;
+@{dedicated_maps}
+  return t;
+}
+
+static std::unordered_map<std::string, std::string> const* CreateRenameConvertMap@{version_pair}() {
+  using namespace std;
+  auto t = new unordered_map<string, string>();
+  auto &s = *t;
+@{rename_maps}
+  return t;
+}
+)";
 } // namespace
+
+class Version {
+public:
+  explicit Version(std::string const &v) {
+    auto tokens = Split(v, '.');
+    if (tokens.size() < 2) {
+      throw std::runtime_error("invalid version format: " + v);
+    }
+    fMajor = std::stoi(tokens[0]);
+    fMinor = std::stoi(tokens[1]);
+    if (tokens.size() > 2) {
+      fBugfix = std::stoi(tokens[2]);
+    }
+    if (tokens.size() > 3) {
+      throw std::runtime_error("invalid version format: " + v);
+    }
+  }
+
+  std::string toString() const {
+    std::string ret = std::to_string(fMajor) + "." + std::to_string(fMinor);
+    if (fBugfix) {
+      ret += "." + std::to_string(*fBugfix);
+    }
+    return ret;
+  }
+
+  bool equals(Version const &other) const {
+    if (fMajor != other.fMajor) {
+      return false;
+    }
+    if (fMinor != other.fMinor) {
+      return false;
+    }
+    if (!fBugfix && !other.fBugfix) {
+      return true;
+    }
+    if (fBugfix && other.fBugfix) {
+      return *fBugfix == *other.fBugfix;
+    } else {
+      return false;
+    }
+  }
+
+  int fMajor;
+  int fMinor;
+  std::optional<int> fBugfix;
+};
 
 int main(int argc, char *argv[]) {
   using namespace std;
@@ -43,13 +198,8 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  ofstream out(argv[1]);
-  out << "#pragma once" << endl;
-  out << endl;
-  out << "namespace je2be::via {" << endl;
-  out << endl;
-  out << "class BackwardConverter {" << endl;
-  out << "  BackwardConverter() = delete;" << endl;
+  vector<pair<Version, Version>> versionPairs;
+  set<string> versions;
 
   for (auto item : fs::directory_iterator(directory)) {
     if (!item.is_regular_file()) {
@@ -60,20 +210,60 @@ int main(int argc, char *argv[]) {
     if (filename.extension().string() != ".json" || !filename.string().starts_with("mapping-")) {
       continue;
     }
-    cout << p << endl;
-    ifstream ifs(p.string());
-    auto obj = nlohmann::json::parse(ifs);
-    auto found = obj.find("blockstates");
-    if (found == obj.end()) {
-      continue;
+    auto pair = filename.replace_extension().string().substr(8);
+    auto indexTo = pair.find_first_of("to");
+    if (indexTo == string::npos) {
+      throw runtime_error("invalid version pair format: " + pair);
     }
-    auto const &blockstates = found.value();
-    cout << blockstates << endl;
+    auto toVersionString = pair.substr(0, indexTo);
+    auto fromVersionString = pair.substr(indexTo + 2);
+    Version to(toVersionString);
+    Version from(fromVersionString);
+    versionPairs.push_back(make_pair(from, to)); // filename: 1.18to1.19, make_pair(1.19, 1.18)
+    versions.insert(from.toString());
+    versions.insert(to.toString());
   }
 
-  out << "};" << endl;
-  out << endl;
-  out << "} // namespace::via" << endl;
+  sort(versionPairs.begin(), versionPairs.end(), [](auto const &pairA, auto const &pairB) {
+    Version const &a = pairA.first;
+    Version const &b = pairB.first;
+
+    if (a.fMajor == b.fMajor) {
+      if (a.fMinor == b.fMinor) {
+        if (a.fBugfix && b.fBugfix) {
+          return *a.fBugfix > *b.fBugfix;
+        } else if (a.fBugfix) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return a.fMinor > b.fMinor;
+      }
+    } else {
+      return a.fMajor > b.fMajor;
+    }
+  });
+
+  for (int i = 0; i < versionPairs.size() - 1; i++) {
+    auto from = versionPairs[i].second;
+    auto to = versionPairs[i + 1].first;
+    if (!from.equals(to)) {
+      throw runtime_error("version numbers are not consecutive");
+    }
+  }
+
+  int enumValue = 1;
+  ostringstream enumDefinition;
+  for (auto const &v : versionPairs) {
+    enumDefinition << "Version" << Replace(v.first.toString(), ".", "_") << " = " << enumValue << "," << endl;
+    enumValue++;
+  }
+  enumDefinition << "Version" << Replace(versionPairs.back().second.toString(), ".", "_") << " = " << enumValue << "," << endl;
+
+  ofstream out(argv[1]);
+  string src = Replace(classFormat, "@{enum_definition}", Indent(Trim(enumDefinition.str()), "    "));
+  out << Trim(src) << endl;
 
   return 0;
 }
